24bbe1c03f43c9adb656441ebe27bb6b3e696421
RRKallan <r.kallan@upcmail.nl>
2017-11-13 22:04:58 +0100

    Add files via upload

---
+++ jsonDB/src/server/router/validate-data.js
@@ -0,0 +1,20 @@
+const _ = require('lodash')
+
+function validateKey(key) {
+  if (key.indexOf('/') !== -1) {
+    const msg = [
+      `Oops, found / character in database property '${key}'.`
+    ].join('\n')
+    throw new Error(msg)
+  }
+}
+
+module.exports = obj => {
+  if (_.isPlainObject(obj)) {
+    Object.keys(obj).forEach(validateKey)
+  } else {
+    throw new Error(
+      `Data must be an object. Found ${typeof obj}.`
+    )
+  }
+}
