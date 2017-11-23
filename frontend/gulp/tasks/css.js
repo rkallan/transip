d5a9055be78e44406c764ceb1147945d8280a43e
rrkallan <rrkallan@gmail.com>
2017-11-23 09:08:42 +0100

    wip

--- frontend/gulp/tasks/css.js
+++ frontend/gulp/tasks/css.js
@@ -1,20 +1,21 @@
-module.exports = function(gulp, data, util, taskName){
-	'use strict';
-	
-	const autoprefixer = require('gulp-autoprefixer');
-	const sass = require('gulp-sass');
-	
-	gulp.task(taskName + ':compile', function() {
-		return gulp.src([data.someCfg.conf.path.dev.resources.scss + '**/*.{scss,sass}'])
-			.pipe(sass().on('error', sass.logError))
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions']
-			}))
-			.pipe(gulp.dest(data.someCfg.conf.path.dest.resources.css));
-	});
-	
-	gulp.task('css:watch', ['css:compile'], function() {
-		return global.browserSync.reload('*.css');
-	});
+module.exports = function(gulp, data, util, taskName) {
+  'use strict';
+
+  const autoprefixer = require('gulp-autoprefixer');
+  const sass = require('gulp-sass');
+
+  gulp.task(taskName + ':compile', function() {
+    return gulp.src([data.someCfg.conf.path.dev.resources.scss + '**/*.{scss,sass}'])
+      .pipe(sass()
+        .on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest(data.someCfg.conf.path.dest.resources.css));
+  });
+
+  gulp.task('css:watch', ['css:compile'], function() {
+    return global.browserSync.reload('*.css');
+  });
 
 };
